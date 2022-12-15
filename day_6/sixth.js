35b0e0941e5a84726719dbc7a05b07b7e3539f52
Olesia Guidi <olgarbuzova@gmail.com>
2022-12-15 17:41:27 -0500

    question 2 day 6

--- day_6/sixth.js
+++ day_6/sixth.js
@@ -3,7 +3,8 @@ var fs = require('fs');
 fs.readFile('./input.txt', 'utf8', function (err, data) {
 	if (err) throw err;
 	let arr = data.split('');
-	console.log('result', findMarker(arr));
+	// console.log('result', findMarker(arr));
+  console.log('result 2', findMarker14(arr))
 	return;
 });
 
@@ -11,6 +12,21 @@ function findMarker(arr) {
 	let markerCheck = [];
 	for (let i = 0; i < arr.length; i++) {
 		if (markerCheck.length === 4) return i;
+		if (!markerCheck.includes(arr[i])) {
+			markerCheck.push(arr[i]);
+		} else {
+			markerCheck = markerCheck.slice(markerCheck.indexOf(arr[i])+1);
+      markerCheck.push(arr[i]);
+		}
+	}
+}
+
+//question 2
+
+function findMarker14(arr) {
+	let markerCheck = [];
+	for (let i = 0; i < arr.length; i++) {
+		if (markerCheck.length === 14) return i;
 		if (!markerCheck.includes(arr[i])) {
 			markerCheck.push(arr[i]);
 		} else {
